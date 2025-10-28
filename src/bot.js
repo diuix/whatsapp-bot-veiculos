acbd4d92d3c3a4ced8c737d43c780e4eca72abb5
Diuix <diuixsoftware@gmail.com>
2025-10-28 19:05:18 -0400

    Corrigir configuração Puppeteer para Railway

--- src/bot.js
+++ src/bot.js
@@ -8,7 +8,16 @@ class WhatsAppBot {
             authStrategy: new LocalAuth(),
             puppeteer: {
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--single-process',
+                    '--disable-gpu'
+                ]
             }
         });
         
